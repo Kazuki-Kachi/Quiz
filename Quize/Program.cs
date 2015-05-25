497f168e2c6dbd7e7eeb58b14005bfffac653ce7
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-05-25 16:11:07 +0900

    Add object

--- Quize/Program.cs
+++ Quize/Program.cs
@@ -14,23 +14,38 @@ namespace Quize
             var s = new MyStruct() { Value = 10 };
             IInterface ic = new MyClass() { Value = 10 };
             IInterface @is = new MyStruct() { Value = 10 };
+            object oc = new MyClass() { Value = 10 };
+            object os = new MyStruct() { Value = 10 };
             SetValue(c);
             SetValue(s);
             SetValue(ic);
             SetValue(@is);
+            SetValue(oc);
+            SetValue(os);
 
             WriteLine($"class:{c.ToString()}");
+            WriteLine("-------------------------------");
             WriteLine($"struct:{s.ToString()}");
+            WriteLine("-------------------------------");
             WriteLine($"interface(class):{ic.ToString()}");
+            WriteLine("-------------------------------");
             WriteLine($"interface(struct):{@is.ToString()}");
+            WriteLine("-------------------------------");
+            WriteLine($"object(class):{oc.ToString()}");
+            WriteLine("-------------------------------");
+            WriteLine($"object(struct):{os.ToString()}");
         }
 
         static void SetValue(IInterface target) => target.Value = 100;
         static void SetValue(MyClass target) => target.Value = 100;
         static void SetValue(MyStruct target) => target.Value = 100;
-
+        static void SetValue(object target)
+        {
+            var t = target as IInterface;
+            if (t == null) return;
+            t.Value = 100;
+        }
     }
-
     interface IInterface
     {
         int Value { get; set; }
