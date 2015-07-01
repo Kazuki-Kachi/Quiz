11d07e3d4293c046a2b2a6e29e3a23959a697fec
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-07-02 00:23:05 +0900

    add Second

---
+++ Second/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Linq.Enumerable;
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question<int>(null);
+            Question(Empty<int>());
+            Question(Range(0, 1));
+        }
+
+        static void Question<T>(IEnumerable<T> values)
+        {
+            try
+            {
+                WriteLine(values?.All(_ => false).ToString());
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"Type:{ex.GetType().Name}");
+                WriteLine($"Message:{ex.Message}");
+            }
+        }
+
+    }
+}
